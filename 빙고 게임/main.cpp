a2170b1e471b93936e44aada994f167048a25c11
kthss01 <kthss01@naver.com>
2018-01-23 21:11:26 +0900

    자동화 및 빙고시 기록 저장하여 게임 종료 후 빙고 기록 보여주기

--- 빙고 게임/main.cpp
+++ 빙고 게임/main.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+typedef struct Storage {
+	int board[5][5];
+	int bingo;
+	int gameCount;
+} Store;
+
 // 빙고판 출력하는 함수
 // 2차원 배열 포인터로 받은거임
 void showBoard(int board[5][5]);
@@ -14,6 +20,11 @@ void changeBoard(int board[5][5], int number);
 int checkBoard(int board[5][5]);
 // 빙고판에 숫자가 있는지 확인하는 함수
 bool checkNumber(int board[5][5], int number);
+
+// 빙고 상태를 저장소 구조체의 저장하는 함수
+void saveBoard(Store* store, int* storeSize, int bingo, int board[5][5], int gameCount);
+// 빙고 상태를 저장소 구조체로부터 읽어오는 함수
+void loadBoard(Store* store, int storeSize);
 
 // 2차원 배열 변경시 반영되는지 테스트
 // 2차원 배열 변경시 반영됨
@@ -36,6 +47,9 @@ void main() {
 	int bingo = 0;
 	int oldBingo = 0; // 이전 빙고값을 저장하는 변수
 	int gameCount = 0;
+
+	Store store[5];
+	int storeSize = 0;
 
 	// 숫자 초기화
 	for (int i = 0; i < 25; i++)
@@ -71,17 +85,14 @@ void main() {
 	// 게임 시작
 	while (true) {
 		system("cls");
-		
-		cout << gameCount << "번째 게임" << endl;
 
-		// 빙고판 출력
-		cout << endl;
-		showBoard(board);
-		cout << endl;
+		cout << gameCount << "번째 게임" << endl;
 
 		// 숫자 입력
 		cout << "숫자를 입력하세요 : ";
-		cin >> selectNumber;
+		// 자동화를 위해 gameCount+1을 그냥 넣어줌
+		selectNumber = gameCount + 1;
+		//cin >> selectNumber;
 		
 		// 숫자 입력 예외 처리
 		if (selectNumber > 26 || selectNumber <= 0) {
@@ -100,20 +111,33 @@ void main() {
 		// 빙고판 변경
 		changeBoard(board, selectNumber);
 
+		// 빙고판 출력
+		cout << endl;
+		showBoard(board);
+		cout << endl;
+
 		// 빙고 확인
 		oldBingo = bingo;
 		bingo = checkBoard(board);
 
-		if (bingo == 5) {
+		if (bingo >= 5) {
 			cout << "5 빙고 완성, 게임 승리" << endl;
+			
+			// 저장해두었던 빙고 상태 읽어오기
+			loadBoard(store, storeSize);
+
 			break;
 		}
 		else if (bingo != oldBingo) {
 			cout << bingo << " 빙고 완성, " << 5 - bingo << " 빙고 남았습니다." << endl;
-			Sleep(1000);
+			Sleep(1000);	// 자동으로 돌리면 sleep 동안에도 진행중이라 위에 문구 안보이는거
+
 			// 잠깐 동안 멈추기위해 입력값 기다리는 방법
 			//getchar(); // endl 남아있어서 한번더 입력받은거
 			//getchar();
+
+			// 빙고 변화가 있으면 빙고판과 게임 횟수 저장하기
+			saveBoard(store, &storeSize, bingo, board, gameCount);
 		}
 
 		gameCount++;
@@ -198,4 +222,47 @@ bool checkNumber(int board[5][5], int number) {
 	}
 	// 숫자가 없으면 false 리턴
 	return false;
+}
+
+// 빙고 상태를 저장소 구조체의 저장하는 함수
+void saveBoard(Store* store, int* storeSize, int bingo, int board[5][5], int gameCount) {
+	// 저장소에 빙고 상태 저장
+	store[*storeSize].bingo = bingo;
+
+	// 저장소에 빙고판 저장
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			store[*storeSize].board[i][j] = board[i][j];
+		}
+	}
+
+	// 저장소에 빙고일때 게임판 수 저장
+	store[*storeSize].gameCount = gameCount;
+
+	// 스토어 인덱스 증가
+	*storeSize = *storeSize + 1;
+}
+
+// 빙고 상태를 저장소 구조체로부터 읽어오는 함수
+void loadBoard(Store* store, int storeSize) {
+	for (int i = 0; i < 35; i++)
+		cout << "*";
+	cout << endl;
+
+	cout << "빙고 기록표" << endl;
+
+	for (int i = 0; i < storeSize; i++) {
+		cout << endl;
+		cout << store[i].gameCount << "번째 게임" << endl;
+
+		cout << endl;
+		showBoard(store[i].board);
+		cout << endl;
+
+		cout << store[i].bingo << " 빙고 완성, " << 5 - store[i].bingo << " 빙고 남음" << endl;
+	}
+
+	for (int i = 0; i < 35; i++)
+		cout << "*";
+	cout << endl;
 }
